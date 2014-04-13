bb318790cdc624a1f6269905c2c9148b4ae62256
Dave Miller <github@jusdave.net>
2014-04-13 02:55:20 -0400

    Add a placeholder for the public-facing page

--- oadueslookup.php
+++ oadueslookup.php
@@ -35,6 +35,7 @@
  */
 
 add_action( 'admin_menu', 'oadueslookup_plugin_menu' );
+add_action( 'parse_request', 'oadueslookup_url_handler' );
 register_activation_hook( __FILE__, 'oadueslookup_install' );
 
 global $oadueslookup_db_version;
@@ -120,6 +121,95 @@ function oadueslookup_install() {
         // updates are done, update the schema version to say we did them
         update_option( "oadueslookup_db_version", $oadueslookup_db_version );
     }
+}
+
+function oadueslookup_user_page( &$wp ) {
+    $content = "This is a placeholder for the dues page.";
+    return $content;
+}
+
+function oadueslookup_url_handler( &$wp ) {
+    global $oadueslookup_body;
+    if($wp->request == 'dues') {
+        # http://stackoverflow.com/questions/17960649/wordpress-plugin-generating-virtual-pages-and-using-theme-template
+        add_action('template_redirect', 'oadueslookup_template_redir');
+        $oadueslookup_body = oadueslookup_user_page($wp);
+        add_filter('the_posts', 'oadueslookup_dummypost');
+        remove_filter('the_content', 'wpautop');
+    }
+}
+
+function oadueslookup_dummypost($posts) {
+    // have to create a dummy post as otherwise many templates
+    // don't call the_content filter
+    global $wp, $wp_query, $oadueslookup_body;
+
+    //create a fake post intance
+    $p = new stdClass;
+    // fill $p with everything a page in the database would have
+    $p->ID = -1;
+    $p->post_author = 1;
+    $p->post_date = current_time('mysql');
+    $p->post_date_gmt =  current_time('mysql', $gmt = 1);
+    $p->post_content = $oadueslookup_body;
+    $p->post_title = 'Dues';
+    $p->post_excerpt = '';
+    $p->post_status = 'publish';
+    $p->ping_status = 'closed';
+    $p->post_password = '';
+    $p->post_name = 'dues_page'; // slug
+    $p->to_ping = '';
+    $p->pinged = '';
+    $p->modified = $p->post_date;
+    $p->modified_gmt = $p->post_date_gmt;
+    $p->post_content_filtered = '';
+    $p->post_parent = 0;
+    $p->guid = get_home_url('/' . $p->post_name); // use url instead?
+    $p->menu_order = 0;
+    $p->post_type = 'page';
+    $p->post_mime_type = '';
+    $p->comment_status = 'closed';
+    $p->comment_count = 0;
+    $p->filter = 'raw';
+    $p->ancestors = array(); // 3.6
+
+    // reset wp_query properties to simulate a found page
+    $wp_query->is_page = TRUE;
+    $wp_query->is_singular = TRUE;
+    $wp_query->is_home = FALSE;
+    $wp_query->is_archive = FALSE;
+    $wp_query->is_category = FALSE;
+    unset($wp_query->query['error']);
+    $wp->query = array();
+    $wp_query->query_vars['error'] = '';
+    $wp_query->is_404 = FALSE;
+
+    $wp_query->current_post = $p->ID;
+    $wp_query->found_posts = 1;
+    $wp_query->post_count = 1;
+    $wp_query->comment_count = 0;
+    // -1 for current_comment displays comment if not logged in!
+    $wp_query->current_comment = null;
+    $wp_query->is_singular = 1;
+
+    $wp_query->post = $p;
+    $wp_query->posts = array($p);
+    $wp_query->queried_object = $p;
+    $wp_query->queried_object_id = $p->ID;
+    $wp_query->current_post = $p->ID;
+    $wp_query->post_count = 1;
+
+    return array($p);
+}
+
+function oadueslookup_template_redir() {
+    # By including the "dues" subtag here, if someone wanted to re-theme this
+    # page, they could, by creating a "page-dues.php" templates file. Otherwise
+    # it just uses page.php.
+    get_template_part('page', 'dues');
+    # we're done, because the above should have displayed the entire page, so
+    # quit instead of letting WP try to display it again.
+    exit;
 }
 
 function oadueslookup_plugin_menu() {

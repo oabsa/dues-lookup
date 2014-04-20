2f000795ccdf50d514e53eb4d7b9c6f4aa74c087
Dave Miller <github@justdave.net>
2014-04-20 18:38:35 -0400

    Resolves #7: hook up update system so it'll auto-update

--- oadueslookup.php
+++ oadueslookup.php
@@ -33,10 +33,26 @@ add_action( 'plugins_loaded', 'oadueslookup_update_db_check' );
 register_activation_hook( __FILE__, 'oadueslookup_install' );
 register_activation_hook( __FILE__, 'oadueslookup_install_data' );
 add_action( 'wp_enqueue_scripts', 'oadueslookup_enqueue_css' );
+add_action( 'init', 'oadueslookup_plugin_updater_init' );
 
 function oadueslookup_enqueue_css() {
     wp_register_style( 'oadueslookup-style', plugins_url('style.css', __FILE__) );
     wp_enqueue_style('oadueslookup-style');
+}
+
+function oadueslookup_plugin_updater_init() {
+    /* Load Plugin Updater */
+    require_once( trailingslashit( plugin_dir_path( __FILE__ ) ) . 'includes/plugin-updater.php' );
+
+    /* Updater Config */
+    $config = array(
+        'base'      => plugin_basename( __FILE__ ), //required
+        'repo_uri'  => 'http://www.justdave.net/dave/',
+        'repo_slug' => 'oadueslookup',
+    );
+
+    /* Load Updater Class */
+    new OADuesLookup_Plugin_Updater( $config );
 }
 
 global $oadueslookup_db_version;
